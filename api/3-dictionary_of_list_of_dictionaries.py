86c22148b27f3a1408e5aaeae83b496619991ef6
Nicoou <nf9600362@gmail.com>
2023-08-22 15:39:27 -0700

    task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Employee ID-based TODO List Progress"""
+
+if __name__ == "__main__":
+    import json
+    from urllib import request
+    import sys
+
+    dic_user = {}
+    url = request.urlopen(
+        "https://jsonplaceholder.typicode.com/users/")
+
+    data = json.loads(url.read().decode("utf-8"))
+
+    url_task = request.urlopen(
+        "https://jsonplaceholder.typicode.com/todos")
+    task = json.loads(url_task.read().decode("utf-8"))
+
+    for user in data:
+        user_id = str(user["id"])
+        user_task = [
+            {
+                "task": task["title"],
+                "username": user["username"],
+                "completed": task["completed"]
+            }
+            for tasks in task if tasks["userId"] == user["id"]
+        ]
+        dic_user[user_id] == user_task
+        with open(f"todo_all_employees.json", "w+") as file:
+            file.write(json.dumps(dic_user))
+            file.close()
