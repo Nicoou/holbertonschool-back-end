4747df060313423149517d2c35fb85cfb78ec562
Nicoou <nf9600362@gmail.com>
2023-08-22 15:00:22 -0700

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,27 +3,27 @@
 
 if __name__ == "__main__":
     import json
-    from urllib import request
+    import requests
     import csv
     import sys
 
     empid = sys.argv[1]
 
-    url = request.urlopen("https://jsonplaceholder.typicode.com/users/{}"
+    url = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                           .format(empid))
 
-    data = json.loads(url.read().decode("utf-8"))
+    data = json.loads(url.text)
 
-    url_task = request.urlopen(
+    url_task = requests.get(
         "https://jsonplaceholder.typicode.com/todos?userId={}".format(empid)
     )
 
-    task = json.loads(url_task.read().decode("utf-8"))
+    task = json.loads(url_task.text)
 
     csv_filename = f"{empid}.csv"
 
     with open(csv_filename, "w", newline="") as csv_file:
-        csv_writer = csv.writer(csv_file, dialect="Dialect")
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         csv_writer.writerow(
             ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
         )
