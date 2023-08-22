375a1d15b63b1f8c5126b8e8156c011893ccc080
Nicoou <nf9600362@gmail.com>
2023-08-22 15:27:35 -0700

    task 2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -9,22 +9,21 @@ if __name__ == "__main__":
 
     empid = sys.argv[1]
 
-    url = request.urlopen("https://jsonplaceholder.typicode.com/users/{}"
-                          .format(empid))
+    url = request.urlopen(
+        "https://jsonplaceholder.typicode.com/users/{}".format(empid))
 
     data = json.loads(url.read().decode("utf-8"))
 
     url_task = request.urlopen(
         "https://jsonplaceholder.typicode.com/todos?userId={}".format(empid)
     )
-
-    task = json.loads(url_task.read().decode("utf-8"))
+    tasks = json.loads(url_task.read().decode("utf-8"))
 
     index = "{}".format(data["id"])
     dic_user = {
         index: []
     }
-    for task in task:
+    for task in tasks:
         dic_user[index].append({
             "task": task["title"],
             "completed": task["completed"],
