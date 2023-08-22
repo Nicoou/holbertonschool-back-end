89e202a02ae54dc01c17d03e196f155ba98553d4
Nicoou <nf9600362@gmail.com>
2023-08-22 15:56:48 -0700

    task 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -4,29 +4,27 @@
 if __name__ == "__main__":
     import json
     from urllib import request
-    import sys
 
     dic_user = {}
-    url = request.urlopen(
-        "https://jsonplaceholder.typicode.com/users/")
+    url = request.urlopen("https://jsonplaceholder.typicode.com/users/")
 
     data = json.loads(url.read().decode("utf-8"))
 
-    url_task = request.urlopen(
-        "https://jsonplaceholder.typicode.com/todos")
+    url_task = request.urlopen("https://jsonplaceholder.typicode.com/todos")
     task = json.loads(url_task.read().decode("utf-8"))
 
     for user in data:
         user_id = str(user["id"])
         user_tasks = [
             {
-                "task": tasks["title"],
+                "task": task["title"],
                 "username": user["username"],
-                "completed": tasks["completed"]
+                "completed": task["completed"],
             }
-            for tasks in task if tasks["userId"] == user["id"]
+            for tasks in task
+            if tasks["userId"] == user["id"]
         ]
         dic_user[user_id] == user_tasks
-        with open(f"todo_all_employees.json", "w+") as file:
-          file.write(json.dumps(dic_user))
-          file.close()
+    with open(f"todo_all_employees.json", "w+") as file:
+        file.write(json.dumps(dic_user))
+        file.close()
